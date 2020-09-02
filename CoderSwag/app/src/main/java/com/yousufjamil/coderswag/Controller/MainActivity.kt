cd7f8d979b3451a2dc614bc533d8e060473452e0
Muhammad Yousuf Jamil <myousufjamil@hotmail.com>
2020-09-02 20:16:30 +0300

    lesson 01

--- CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Controller/MainActivity.kt
+++ CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.yousufjamil.coderswag
+package com.yousufjamil.coderswag.Controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.yousufjamil.coderswag.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
