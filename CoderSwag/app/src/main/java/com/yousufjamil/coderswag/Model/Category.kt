00f3e0ed37f589d569cbc8b3a5bcea95741c2b7c
Muhammad Yousuf Jamil <myousufjamil@hotmail.com>
2020-09-05 15:03:14 +0300

    lesson 02

--- CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Model/Category.kt
+++ CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package com.yousufjamil.coderswag.Model
 
-class Category (val title: String, val image: String)
+class Category(val title: String, val image: String) {
+    override fun toString(): String {
+        return title
+    }
+}
