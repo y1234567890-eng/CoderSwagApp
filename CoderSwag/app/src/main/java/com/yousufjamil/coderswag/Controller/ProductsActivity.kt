eea5ad34c0c195791eb5d6383528289f8161e8a8
Muhammad Yousuf Jamil <myousufjamil@hotmail.com>
2020-10-01 23:25:29 +0300

    lesson 8

--- CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Controller/ProductsActivity.kt
+++ CoderSwag/app/src/main/java/com/yousufjamil/coderswag/Controller/ProductsActivity.kt
@@ -1,15 +1,42 @@
 package com.yousufjamil.coderswag.Controller
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.yousufjamil.coderswag.Adapters.ProductsAdapter
 import com.yousufjamil.coderswag.R
+import com.yousufjamil.coderswag.Services.DataService
 import com.yousufjamil.coderswag.Utilities.EXTRA_CATEGORY
+import kotlinx.android.synthetic.main.activity_products.*
+
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter : ProductsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType.toString()))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(applicationContext, spanCount)
+        productsListView.layoutManager = layoutManager
+        productsListView.adapter = adapter
+
     }
 }
